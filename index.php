5c9d65a9a6ceb68ac173bc11f21e7ba727e5198c
Alex <heretik4@gmail.com>
2019-02-27 18:58:29 +0300

    v-27.02-1

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require 'phpQuery.php';
+for($p=1; $p <= 10; $p++) {
+    $url = 'https://www.webcamrips.com/page/' . $p;
+    $file = file_get_contents($url);
+    $doc = phpQuery::newDocument($file);
+    foreach ($doc->find('.pst-cn h3') as $sec) {
+        $sec = pq($sec);
+        $link = $sec->find('a')->attr('href');
+        $fr = file_get_contents($link);
+        $inner = phpQuery::newDocument($fr);
+        $video = $inner->find('iframe')->attr('src');
+        echo "$video <br>";
+    };
+};
+// $p - page***** грабим со 1 по 5 стр по умолчанию
+// clma06c04d03
