d82e3f064fe6692d21dd0e699fdd58e34c15b123
Alexey AS <heretik4@gmail.com>
2019-03-14 21:04:12 +0300

    wbch-norepeat

--- index.php
+++ index.php
@@ -15,11 +15,17 @@ $parser = new Parser();
 $parser->pars($url,$start, $end);
 file_put_contents("file.txt",PHP_EOL, FILE_APPEND);
 
-//$end_time = microtime(true);
-//$runtime = $end_time - $start_time;
-//echo "Время выполнения скрипта в мс: " . $runtime;
+$repeat = file('file.txt');
+$norepeat = array_unique($repeat, SORT_STRING);
+file_put_contents("file-clear.txt",$norepeat, FILE_APPEND);
+
+/*$end_time = microtime(true);
+$runtime = $end_time - $start_time;
+echo "Время выполнения скрипта в мс: " . $runtime;
+*/
+
 
 //если мы хотим скачать только 1ю страницу, то start=0 end=1
 //не нравится - перепиши условее в Parser.php в строке 6 на <=
-//если не нужна дата - удали строку 14 и 17 строку
-//если не нужна проверка скорости выполнения - удали 7, 18-20 строку
+//Дата в file.txt - строка 13 и 16
+//Проверка скорости выполнения - строка 7, 22-24
